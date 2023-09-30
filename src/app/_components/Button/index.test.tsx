c75c6e29e0f32d1eafcfb9900734f71d0a93ee55
mk <mk.eng1119@gmail.com>
2023-10-01 01:10:10 +0900

    Revert "chore: add baseUrl"
    This reverts commit b1886c507fe6968064262d93d97472e967fa5fef.

--- src/app/_components/Button/index.test.tsx
+++
@@ -1,7 +0,0 @@
-import { render, screen } from '@testing-library/react'
-import { Button } from '.'
-
-test('role="button"', () => {
-  render(<Button>Button</Button>)
-  expect(screen.getByRole('button')).toBeInTheDocument()
-})
