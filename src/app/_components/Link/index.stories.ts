032c15b5312d8ebb77671467995f8c97f262d3d5
mk <mk.eng1119@gmail.com>
2023-10-01 01:44:08 +0900

    add: Link Component & test & story

---
+++ src/app/_components/Link/index.stories.ts
@@ -0,0 +1,35 @@
+import { Link } from '.'
+import type { Meta, StoryObj } from '@storybook/react'
+
+const meta: Meta<typeof Link> = {
+  title: 'ui-elements/Link',
+  component: Link,
+  tags: ['autodocs']
+}
+
+export default meta
+type Story = StoryObj<typeof Link>
+
+export const Small: Story = {
+  args: {
+    href: '#',
+    size: 'sm',
+    children: 'Link'
+  }
+}
+
+export const Medium: Story = {
+  args: {
+    href: '#',
+    size: 'md',
+    children: 'Link'
+  }
+}
+
+export const Large: Story = {
+  args: {
+    href: '#',
+    size: 'lg',
+    children: 'Link'
+  }
+}
---
+++ src/app/_components/Link/index.tsx
@@ -0,0 +1,30 @@
+import * as React from 'react'
+import { VariantProps, cva } from 'cva'
+import NextLink from 'next/link'
+import { combineStyles } from '@/app/_libs/style'
+
+export const linkVariants = cva({
+  base: 'text-black underline-offset-[6px] hover:underline disabled:pointer-events-none disabled:opacity-50',
+  variants: {
+    size: {
+      sm: 'text-sm',
+      md: 'text-base',
+      lg: 'text-lg'
+    }
+  },
+  defaultVariants: {
+    size: 'md'
+  }
+})
+type Props = React.ComponentPropsWithoutRef<typeof NextLink> & VariantProps<typeof linkVariants>
+
+export const Link = React.forwardRef<HTMLAnchorElement, Props>(function AnchorButtonBase(
+  { children, className, size, ...props },
+  ref
+) {
+  return (
+    <NextLink className={combineStyles(linkVariants({ size, className }))} ref={ref} {...props}>
+      {children}
+    </NextLink>
+  )
+})
