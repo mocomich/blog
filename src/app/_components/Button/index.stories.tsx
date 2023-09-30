85111b1732ab774e109edc079938e90b0163edc7
mk <mk.eng1119@gmail.com>
2023-10-01 01:13:16 +0900

    add: Button Component & test & story

---
+++ src/app/_components/Button/index.stories.tsx
@@ -0,0 +1,86 @@
+import { Button } from '.'
+import type { Meta, StoryObj } from '@storybook/react'
+
+const meta: Meta<typeof Button> = {
+  title: 'ui-elements/Button',
+  component: Button,
+  tags: ['autodocs']
+}
+
+export default meta
+type Story = StoryObj<typeof Button>
+
+export const Small: Story = {
+  args: {
+    variant: 'primary',
+    size: 'sm',
+    children: 'Button',
+    isLoading: false
+  }
+}
+
+export const Medium: Story = {
+  args: {
+    variant: 'primary',
+    size: 'md',
+    children: 'Button'
+  }
+}
+
+export const Large: Story = {
+  args: {
+    variant: 'primary',
+    size: 'lg',
+    children: 'Button'
+  }
+}
+
+export const Primary: Story = {
+  args: {
+    variant: 'primary',
+    children: 'Button'
+  }
+}
+
+export const Secondary: Story = {
+  args: {
+    variant: 'secondary',
+    children: 'Button'
+  }
+}
+
+export const Tertiary: Story = {
+  args: {
+    variant: 'tertiary',
+    children: 'Button'
+  }
+}
+
+export const Error: Story = {
+  args: {
+    variant: 'error',
+    children: 'Button'
+  }
+}
+
+export const Outline: Story = {
+  args: {
+    variant: 'outline',
+    children: 'Button'
+  }
+}
+
+export const Link: Story = {
+  args: {
+    variant: 'link',
+    children: 'Button'
+  }
+}
+
+export const Loading: Story = {
+  args: {
+    variant: 'primary',
+    children: 'Button',
+    isLoading: true
+  }
+}
