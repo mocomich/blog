37832840b22409d7c9586e8176c168aa64571ce2
mk <mk.eng1119@gmail.com>
2023-10-03 18:56:17 +0900

    add: post type

---
+++ src/app/(posts)/_types/index.ts
@@ -0,0 +1,11 @@
+export type PostType = {
+  body?: string
+  domain: string
+  excerpt: string
+  id: string
+  publishedAt: string
+  tags: string[]
+  title: string
+  updatedAt: string
+  url: string
+}
