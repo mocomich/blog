40208e6d1f30a19242161b5b6c291b0160f67374
mk <mk.eng1119@gmail.com>
2023-10-06 00:05:15 +0900

    chore: change main style

--- src/app/_components/Layout/index.tsx
+++ src/app/_components/Layout/index.tsx
@@ -6,7 +6,7 @@ export const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="relative flex min-h-screen flex-col overflow-hidden">
       <Header />
-      <main className="mx-auto w-full grow px-3 lg:max-w-7xl lg:px-0">{children}</main>
+      <main className="mx-auto w-full grow px-4 lg:max-w-7xl xl:px-0">{children}</main>
       <Footer />
     </div>
   )
