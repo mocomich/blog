a54dbf2c28f8577eceaaef6b14849e35d35413d5
mk <mk.eng1119@gmail.com>
2023-10-08 08:08:56 +0900

    fix: style

--- src/app/_components/Layout/index.tsx
+++ src/app/_components/Layout/index.tsx
@@ -4,7 +4,7 @@ import { Header } from './Header'
 
 export const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="relative flex min-h-screen flex-col overflow-hidden">
+    <div className="relative flex min-h-screen flex-col">
       <Header />
       <main className="mx-auto w-full grow px-4 lg:max-w-7xl xl:px-0">{children}</main>
       <Footer />
