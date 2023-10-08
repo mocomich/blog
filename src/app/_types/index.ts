4791f3375afd4c72509c38372571f15ccb4b8622
mk <mk.eng1119@gmail.com>
2023-10-08 10:41:13 +0900

    add: types

---
+++ src/app/_types/index.ts
@@ -0,0 +1,15 @@
+export type PostType = {
+  body?: string
+  domain: 'tech-vinyl.com' | 'zenn.dev'
+  excerpt: string
+  id: string
+  publishedAt: string
+  tags: string[]
+  title: string
+  updatedAt: string
+  url: string
+}
+
+export type Path = { href: string; title: string }
+
+export type SearchParams = { [key: string]: string | string[] | undefined }
