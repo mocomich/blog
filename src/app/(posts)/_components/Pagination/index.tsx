0be46ec8f526a325ddec85447ef0f0dda902f9b9
mk <mk.eng1119@gmail.com>
2023-10-09 07:08:51 +0900

    update: for change server-component

--- src/app/(posts)/_components/Pagination/index.tsx
+++ src/app/(posts)/_components/Pagination/index.tsx
@@ -1,9 +1,6 @@
-'use client'
-
 import { ChevronRightIcon, ChevronLeftIcon } from '@heroicons/react/24/outline'
 
 import clsx from 'clsx'
-import { usePathname } from 'next/navigation'
 import { PerPage } from '@/app/(posts)/_const'
 import { isCurrent } from '@/app/_libs/pagination'
 import { PaginationLink } from './PaginationLink'
@@ -11,29 +8,29 @@ import { PaginationLink } from './PaginationLink'
 type Props = {
   totalCount: number
   current: number
+  basePath: string
 }
 
-export const Pagination = ({ totalCount, current }: Props) => {
-  const pathname = usePathname()
+export const Pagination = ({ totalCount, current, basePath }: Props) => {
   const range = (start: number, end: number) => [...Array(end - start + 1)].map((_, i) => start + i)
 
   return (
     <nav aria-label="ページネーション">
       <ul className="flex justify-center gap-2">
         <li className={clsx('justify-self-start', current > 1 ? '' : 'hidden')}>
-          <PaginationLink aria-label="前のページ" href={`${pathname}?page=${current - 1}`}>
+          <PaginationLink aria-label="前のページ" href={`${basePath}/${current - 1}`}>
             <ChevronLeftIcon className="w-5" />
           </PaginationLink>
         </li>
         {range(1, Math.ceil(totalCount / PerPage)).map((number, index) => (
           <li key={index}>
-            <PaginationLink href={`${pathname}?page=${number}`} {...isCurrent(number === current)}>
+            <PaginationLink href={`${basePath}/${number}`} {...isCurrent(number === current)}>
               {number}
             </PaginationLink>
           </li>
         ))}
         <li className={clsx(current < Math.ceil(totalCount / PerPage) ? '' : 'hidden')}>
-          <PaginationLink aria-label="次のページ" href={`${pathname}?page=${current + 1}`}>
+          <PaginationLink aria-label="次のページ" href={`${basePath}/${current + 1}`}>
             <ChevronRightIcon className="w-5" />
           </PaginationLink>
         </li>
