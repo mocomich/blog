4e7f460920dbc39df7834c147967bd33a87b3f19
mk <mk.eng1119@gmail.com>
2023-10-08 10:40:49 +0900

    chore: change path

--- src/app/_const/index.ts
+++ src/app/_const/index.ts
@@ -1,9 +1,9 @@
-import { Path } from '@/app/(posts)/_types'
+import { Path } from '@/app/_types'
 
 export const PostsNavPaths = [
   { href: '/posts/all', title: 'All' },
-  { href: '/posts/all/vinyl', title: 'Vinyl' },
-  { href: '/posts/all/zenn', title: 'Zenn' }
+  { href: '/posts/vinyl', title: 'Vinyl' },
+  { href: '/posts/zenn', title: 'Zenn' }
 ] as const satisfies Readonly<Path[]>
 
 export const HeaderNavPaths = [
