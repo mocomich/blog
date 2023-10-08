29e480253955804dabf620d8bb5108a6935c4dae
mk <mk.eng1119@gmail.com>
2023-10-09 07:16:11 +0900

    update: rm PostList

--- src/app/(posts)/_components/CardPosts/index.tsx
+++ src/app/(posts)/_components/CardPosts/index.tsx
@@ -1,16 +1,9 @@
 import React from 'react'
 import Link from 'next/link'
-import { notFound } from 'next/navigation'
-import { PerPage } from '@/app/(posts)/_const'
-
-import { getPostList } from '@/app/(posts)/server/posts'
-import { getOffset } from '@/app/_libs/pagination'
-import { querySchema } from '@/app/_schema'
-import { PostType, SearchParams } from '@/app/_types'
-import { Pagination } from '../Pagination'
+import { PostType } from '@/app/_types'
 import { Post } from './Post'
 
-export const Posts = ({ posts, totalCount, current }: { posts: PostType[]; totalCount: number; current: number }) => {
+export const Posts = ({ posts }: { posts: PostType[] }) => {
   return (
     <div className="space-y-20">
       <ul className="grid w-full gap-6 md:grid-cols-2 lg:grid-cols-2 lg:gap-8">
@@ -22,23 +15,6 @@ export const Posts = ({ posts, totalCount, current }: { posts: PostType[]; total
           </li>
         ))}
       </ul>
-      <Pagination totalCount={totalCount} current={current} />
     </div>
   )
-}
-
-export const PostList = async ({
-  searchParams,
-  domain
-}: {
-  searchParams: SearchParams
-  domain?: PostType['domain']
-}) => {
-  const offset = getOffset(searchParams, querySchema)
-  if (offset === undefined) {
-    notFound()
-  }
-  const filters = domain ? `domain[contains]${domain}` : undefined
-  const { contents: posts, totalCount } = await getPostList({ offset: offset * PerPage, limit: PerPage, filters })
-  return <Posts posts={posts} totalCount={totalCount} current={offset + 1} />
 }
