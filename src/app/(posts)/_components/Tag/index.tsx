aeb2a397be296cd8c6ec06a78ca2149e8ad5ed12
mk <mk.eng1119@gmail.com>
2023-10-09 18:47:40 +0900

    update: tag

--- src/app/(posts)/_components/Tag/index.tsx
+++ src/app/(posts)/_components/Tag/index.tsx
@@ -1,16 +1,5 @@
 import * as React from 'react'
-import { PostType } from '../../../_types'
 
 export const Tag = ({ children }: { children: React.ReactNode }) => {
-  return <span className="rounded-full bg-gray-500 px-2 py-0.5 text-xs">{children}</span>
-}
-
-export const TagList = ({ tags }: Pick<PostType, 'tags'>) => {
-  return (
-    <div className="flex flex-wrap gap-2 text-white">
-      {tags.map((tag) => (
-        <Tag key={tag}>{tag}</Tag>
-      ))}
-    </div>
-  )
+  return <span className="rounded-full bg-gray-500 px-2 py-1 text-xs text-white hover:bg-gray-900">{children}</span>
 }
