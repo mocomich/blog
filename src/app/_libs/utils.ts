4766833f2ae9e01c45d40561bdb784919973169c
mk <mk.eng1119@gmail.com>
2023-11-09 01:49:56 +0900

    add: flexibleDateFormat function

--- src/app/_libs/utils.ts
+++ src/app/_libs/utils.ts
@@ -1,3 +1,6 @@
+import { differenceInDays, format, formatDistanceToNow } from 'date-fns'
+import { ja } from 'date-fns/locale'
+
 export function isExist<T>(v: T | null | undefined): v is NonNullable<T> {
   return typeof v !== 'undefined' && v !== null
 }
@@ -6,4 +9,22 @@ export function assertIsExist<T>(v: T | null | undefined, target = ''): asserts 
   if (!isExist(v)) {
     throw new Error(`${target} should be specified`.trim())
   }
+}
+
+export function flexibleDateFormat(date: Date) {
+  const formatDate = (date: Date) => {
+    const diffInDays = differenceInDays(new Date(), date)
+
+    if (diffInDays < 8) {
+      return formatDistanceToNow(date, {
+        locale: ja,
+        addSuffix: true
+      })
+    } else if (diffInDays < 365) {
+      return format(date, 'MM月dd日')
+    } else {
+      return format(date, 'yyyy年MM月dd日')
+    }
+  }
+  return formatDate(date)
 }
