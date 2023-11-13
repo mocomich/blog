945c44d6805c54d5a2b2a8d48b3c3cf91e3d44c3
mk <mk.eng1119@gmail.com>
2023-11-14 00:13:17 +0900

    update: add OpgCard

--- src/app/(posts)/posts/[slug]/_components/Markdown/index.tsx
+++ src/app/(posts)/posts/[slug]/_components/Markdown/index.tsx
@@ -2,47 +2,63 @@ import React from 'react'
 
 import hljs, { AutoHighlightResult } from 'highlight.js'
 import parse, { domToReact, HTMLReactParserOptions, Element, Text } from 'html-react-parser'
+import { getOgpInfo } from '@/app/_libs/ogp'
 import { PostType } from '@/app/_types'
 import { H2, H3, H4 } from '../Heading'
+import { OGPCard } from '../OgpCard'
 import styles from './index.module.css'
 
 type Props = Required<Pick<PostType, 'body'>>
 
-const options: HTMLReactParserOptions = {
-  replace: (domNode) => {
-    if (!(domNode instanceof Element && domNode?.attribs)) {
-      return
-    }
+export const Markdown = async ({ body }: Props) => {
+  const ogpInfo = await getOgpInfo(body)
 
-    if (domNode.name === 'h2') {
-      const id = (domNode.children[0] as Text).data
+  const options: HTMLReactParserOptions = {
+    replace: (domNode) => {
+      if (!(domNode instanceof Element && domNode?.attribs)) {
+        return
+      }
 
-      return <H2 id={id}>{domToReact(domNode.children, options)}</H2>
-    }
-    if (domNode.name === 'h3') {
-      const id = (domNode.children[0] as Text).data
-      return <H3 id={id}>{domToReact(domNode.children, options)}</H3>
-    }
-    if (domNode.name === 'h4') {
-      const id = (domNode.children[0] as Text).data
-      return <H4 id={id}>{domToReact(domNode.children, options)}</H4>
-    }
-    if (domNode.name === 'pre') {
-      const code = ((domNode.children[0] as Element).children[0] as Text).data
-      const highlightCode: AutoHighlightResult = hljs.highlightAuto(code)
-      return (
-        <pre>
-          <code
-            className="hljs" // eslint-disable-line
-          >
-            {parse(highlightCode.value)}
-          </code>
-        </pre>
-      )
+      if (domNode.name === 'h2') {
+        const id = (domNode.children[0] as Text).data
+
+        return <H2 id={id}>{domToReact(domNode.children, options)}</H2>
+      }
+      if (domNode.name === 'h3') {
+        const id = (domNode.children[0] as Text).data
+        return <H3 id={id}>{domToReact(domNode.children, options)}</H3>
+      }
+      if (domNode.name === 'h4') {
+        const id = (domNode.children[0] as Text).data
+        return <H4 id={id}>{domToReact(domNode.children, options)}</H4>
+      }
+      if (domNode.name === 'pre') {
+        const code = ((domNode.children[0] as Element).children[0] as Text).data
+        const highlightCode: AutoHighlightResult = hljs.highlightAuto(code)
+        return (
+          <pre>
+            <code
+              className="hljs" // eslint-disable-line
+            >
+              {parse(highlightCode.value)}
+            </code>
+          </pre>
+        )
+      }
+
+      if (domNode.name === 'a') {
+        const url = (domNode.children[0] as Text).data
+        const ogp = ogpInfo.find((ogp) => ogp.url === url)
+        return (
+          ogp && (
+            <a href={ogp.url} target="_blank" rel="noopener noreferrer" className="block">
+              <OGPCard {...ogp} />
+            </a>
+          )
+        )
+      }
     }
   }
-}
 
-export const Markdown = ({ body }: Props) => {
   return <div className={styles.content}>{parse(body, options)}</div>
 }
